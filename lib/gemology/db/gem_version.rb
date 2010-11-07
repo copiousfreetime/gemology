52b7d5602244d2d0a685c80fa9d7dba059f7744a
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2010-11-07 21:38:07 +0000

    fix missing requirement relation

--- lib/gemology/db/gem_version.rb
+++ lib/gemology/db/gem_version.rb
@@ -4,11 +4,13 @@ module Gemology
       include ::Gemology::Logable
       many_to_one :gem
       one_to_one  :gem_version_raw_specification
-      one_to_many :gem_version_licenses
+      one_to_many :gem_version_files
+
       one_to_many :gem_version_authors
       one_to_many :gem_version_emails
-      one_to_many :gem_version_files
+      one_to_many :gem_version_requirements
       one_to_many :gem_version_dependencies
+      one_to_many :gem_version_licenses
 
       many_to_many :authors,      :join_table => :gem_version_authors
       many_to_many :emails,       :join_table => :gem_version_emails
