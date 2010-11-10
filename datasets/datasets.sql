ede84dc9e69f5941bbb76d971d2da2984d92ba62
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2010-11-10 17:30:41 +0000

    savepoint

--- datasets/datasets.sql
+++ datasets/datasets.sql
@@ -40,7 +40,7 @@ COPY (
           ,count( * )   AS count
       FROM gem_versions 
   GROUP BY release_day_of_month
-  ORDER BY release_day_of_montn
+  ORDER BY release_day_of_month
 ) TO :'output'
 WITH CSV HEADER
 ;
@@ -85,23 +85,23 @@ WITH CSV HEADER
 --
 -- Count of new gems by day
 --
-\set output :prefix/new-gems-by-day.csv
+\set output :prefix/new-gems-by-month.csv
 \echo Writing :output
 
 COPY (
-    WITH first_release_dates AS (
+    WITH first_release_months AS (
          SELECT g.name                 AS name
-               ,min( gv.release_date ) AS first_release_date
+               ,min( date_trunc('month', gv.release_date )) AS first_release_month
            FROM gems AS g
            JOIN gem_versions AS gv
              ON g.id = gv.gem_id
        GROUP BY g.name 
     )
-    SELECT first_release_date AS release_date
+    SELECT first_release_month AS release_month
           ,count(name) AS count
-      FROM first_release_dates
-  GROUP BY release_date
-  ORDER BY release_date
+      FROM first_release_months
+  GROUP BY release_month
+  ORDER BY release_month
 ) TO :'output'
 WITH CSV HEADER
 ;
@@ -110,7 +110,7 @@ WITH CSV HEADER
 --
 -- count by day of week
 --
-\set output :prefix/day-of-week-cunts.csv
+\set output :prefix/day-of-week-counts.csv
 \echo Writing :output
 COPY (
     SELECT extract( isodow from release_date) AS day_of_week
@@ -136,15 +136,15 @@ WITH CSV HEADER
 ;
    
 -- 
--- Count of new authors by day
+-- Count of new authors by month
 --
-\set output :prefix/new-authors-by-day.csv
+\set output :prefix/new-authors-by-month.csv
 \echo Writing :output
 
 COPY (
-    WITH first_release_dates AS (
+    WITH first_release_months AS (
          SELECT a.name                 AS name
-               ,min( gv.release_date ) AS first_release_date
+               ,min( date_trunc('month', gv.release_date )) AS first_release_month
            FROM authors             AS a
            JOIN gem_version_authors AS gva
              ON a.id = gva.author_id
@@ -152,11 +152,11 @@ COPY (
              ON gva.gem_version_id = gv.id
        GROUP BY name
     )
-    SELECT first_release_date AS release_date
+    SELECT first_release_month AS release_month
           ,count(name) AS count
-      FROM first_release_dates
-  GROUP BY release_date
-  ORDER BY release_date
+      FROM first_release_months
+  GROUP BY release_month
+  ORDER BY release_month
 ) TO :'output'
 WITH CSV HEADER
 ;
@@ -179,3 +179,74 @@ COPY (
 WITH CSV HEADER
 ;
 
+--
+-- dependency operator counts
+-- 
+\set output :prefix/dependency-operator-counts.csv
+\echo Writing :output
+COPY (
+    SELECT operator
+          ,count(*) as count 
+      FROM dependencies 
+  GROUP BY operator
+) TO :'output'
+WITH CSV HEADER
+;
+
+--
+-- pre release dependencies
+--
+\set output :prefix/prerelease-dependency-counts.csv
+\echo Writing :output
+COPY ( 
+    SELECT is_prerelease
+          ,count(*) AS count
+      FROM dependencies 
+  GROUP BY is_prerelease 
+) TO :'output'
+WITH CSV HEADER
+;
+
+---
+--- required ruby gems version
+---
+\set output :prefix/required-ruby-version-counts.csv
+\echo Writing :output
+COPY (
+    SELECT split_part(required_ruby_version, ' ', 2) as required_ruby_version
+          ,count( *) as count
+      FROM gem_versions
+  GROUP BY split_part(required_ruby_version, ' ', 2)
+) TO :'output'
+WITH CSV HEADER
+;
+
+\set output :prefix/required-rubygems-version-counts.csv
+\echo Writing :output
+COPY (
+    SELECT split_part(required_rubygems_version, ' ', 2) as required_rubygems_version
+          ,count( *) as count
+      FROM gem_versions 
+  GROUP BY split_part(required_rubygems_version, ' ', 2)
+) TO :'output'
+WITH CSV HEADER
+;
+
+\set output :prefix/packaged-rubygems-version-counts.csv
+\echo Writing :output
+COPY (
+    SELECT btrim( packaged_rubygems_version ) as rubygems_version
+          ,count( *) as count
+    FROM gem_versions
+    GROUP BY rubygems_version
+    HAVING count(*) > 20
+    ORDER BY count DESC
+) TO :'output'
+WITH CSV HEADER
+;
+
+-- \set output :prefix/gem-dependencies.csv
+-- \echo Writing :output
+-- COPY (
+    -- SELECT g.name
+          -- ,d.type
